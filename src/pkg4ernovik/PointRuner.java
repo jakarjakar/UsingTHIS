86b0c6487b5beabf4a874b438eed33d875c5b46c
Artsiom_Berazavik <Artsiom_Berazavik@epbyminw3766.minsk.epam.com>
2016-09-20 17:11:04 +0300

    Remove useless in main class.

--- src/pkg4ernovik/PointRuner.java
+++ src/pkg4ernovik/PointRuner.java
@@ -1,15 +1,7 @@
 package pkg4ernovik;
 
 public class PointRuner {
-    public int x = 0;
-    public int y = 0;
-    
-    //constructor
-    public PointRuner(int a, int b) {
-        x = a;
-        y = b;
-    }
-    
+   
     public static void main(String[] args) {
         Point0 p0 = new Point0(88, 88);
         Point1This p1 = new Point1This(88, 88);
